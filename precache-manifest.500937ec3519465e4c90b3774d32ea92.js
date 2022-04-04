96d96d80b5de0d15e4e2de97d246db1dc67fa53f
DTPsykko <Psychotherapy420@gmail.com>
2022-04-04 10:51:58 -0700

    Updates

---
+++ precache-manifest.500937ec3519465e4c90b3774d32ea92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8200cebd5a48b5e199b",
+    "url": "/facerecognition/static/css/main.32078ff2.chunk.css"
+  },
+  {
+    "revision": "e8200cebd5a48b5e199b",
+    "url": "/facerecognition/static/js/main.e8200ceb.chunk.js"
+  },
+  {
+    "revision": "05f317640d51b82fd187",
+    "url": "/facerecognition/static/js/1.05f31764.chunk.js"
+  },
+  {
+    "revision": "9212bf284b91f16b4f30",
+    "url": "/facerecognition/static/css/2.669199bd.chunk.css"
+  },
+  {
+    "revision": "9212bf284b91f16b4f30",
+    "url": "/facerecognition/static/js/2.9212bf28.chunk.js"
+  },
+  {
+    "revision": "4755047a088c14f13926",
+    "url": "/facerecognition/static/js/3.4755047a.chunk.js"
+  },
+  {
+    "revision": "100f4a8f7794a8be34c5",
+    "url": "/facerecognition/static/js/runtime~main.100f4a8f.js"
+  },
+  {
+    "revision": "935bb6a8b0884b3a161fdd1791bd0500",
+    "url": "/facerecognition/static/media/brain.935bb6a8.png"
+  },
+  {
+    "revision": "69b09acedff8c48af408c94de5508e95",
+    "url": "/facerecognition/index.html"
+  }
+];
